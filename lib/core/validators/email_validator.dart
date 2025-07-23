887ae3ee19ca048d4117b90cbb29f380faa7717d
Pedro Konda <pedrokonda2@gmail.com>
2025-07-23 19:27:26 -0300

    feat(core): add email validator and password validator

---
+++ lib/core/validators/email_validator.dart
@@ -0,0 +1,10 @@
+class EmailValidator {
+  static String? validate(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter an email';
+    } else if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+      return 'Please enter a valid email';
+    }
+    return null;
+  }
+}
---
+++ lib/core/validators/password_validator.dart
@@ -0,0 +1,10 @@
+class PasswordValidator {
+  static String? validate(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a password';
+    } else if (value.length < 6) {
+      return 'Password must be at least 6 characters';
+    }
+    return null;
+  }
+}
